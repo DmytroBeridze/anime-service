376b864207a8a0011017ba11edebe3a9c13f9ebe
DmytroBeridze <98057267+DmytroBeridze@users.noreply.github.com>
2024-04-27 17:42:10 +0300

    loader from every images_2

--- src/components/homeSlider/HomeSlider.js
+++ src/components/homeSlider/HomeSlider.js
@@ -9,6 +9,7 @@ import spinner from "../../resources/gif/Ellipsis-loader.gif";
 import imgStub from "../../resources/img/130906936alAOy7.png";
 
 const HomeSlider = () => {
+  console.log("render");
   const [anime, setAnime] = useState([]);
   const { getAllAnime, loader } = AnimeService();
   useEffect(() => {
@@ -16,7 +17,7 @@ const HomeSlider = () => {
   }, []);
 
   const setAllAnime = () => {
-    console.log("render");
+    // console.log("render");
     getAllAnime(16, 0).then((data) => setAnime(data));
   };
 
@@ -97,12 +98,8 @@ const HomeSlider = () => {
       },
     ],
   };
-  // -----------render slider------
-  const animeRender = (arr) => {
-    return arr.map((data, i) => <Wiev data={data} key={i} />);
-  };
 
-  const AnimeSlides = animeRender(anime);
+  // --------spinner
   const Load = loader ? (
     <div className="main-slider__element">
       <div className="main-slider__card">
@@ -123,89 +120,175 @@ const HomeSlider = () => {
     </div>
   ) : null;
 
+  // -----------render slider------
+  const animeRender = (arr) => {
+    return arr.map((data, i) => (
+      <div className="main-slider__element">
+        <div className="main-slider__card">
+          {Load}
+          {<Wiev data={data} key={i} />}
+        </div>
+      </div>
+    ));
+  };
+
+  const AnimeSlides = animeRender(anime);
+
   return (
     <div className="slider-container  main-slider">
       <Slider {...settings}>
-        {Load}
+        {/* {Load} */}
         {AnimeSlides}
       </Slider>
     </div>
   );
 };
-
+// ---------content--------
 const Wiev = ({ data }) => {
-  // console.log("render");
   return (
-    <div className="main-slider__element">
-      <div className="main-slider__card">
-        <img className="main-slider__img" src={data.poster} alt={data.title} />
-      </div>
-    </div>
+    <img className="main-slider__img" src={data.poster} alt={data.title} />
   );
 };
-
-// const settings = {
-//   dots: true,
-//   infinite: true,
-//   speed: 500,
-//   slidesToShow: 2,
-//   slidesToScroll: 1,
-// };
+// !------------------------------------
 // const HomeSlider = () => {
-//   console.log("Render HomeSlider");
+//   const [anime, setAnime] = useState([]);
 //   const { getAllAnime, loader } = AnimeService();
-//   const [slide, setSlide] = useState([]);
-
-//   const Load = loader ? (
-//     <img
-//       className="slider"
-//       src={spinner}
-//       alt="spin"
-//       style={{
-//         // position: "absolute",
-//         // left: "50%",
-//         // top: "40%",
-//         display: "block",
-//         width: "100px",
-//         height: "100px",
-//       }}
-//     />
-//   ) : null;
-
 //   useEffect(() => {
-//     getAllAnime().then((data) => setSlide(data));
+//     setAllAnime();
 //   }, []);
 
-//   // render slick
-//   const renderSlicl = () => {
-//     return slide.map((elem) => (
-//       <div>
-//         <div className="main-slider__element">
-//           <div className="main-slider__card">
-//             {/* {loader ? <img src={spinner}></img> : null} */}
-//             {<Wiev elem={elem} />}
-//           </div>
+//   const setAllAnime = () => {
+//     console.log("render");
+//     getAllAnime(16, 0).then((data) => setAnime(data));
+//   };
+
+//   // ----------setings slider------
+//   const settings = {
+//     centerMode: true,
+//     infinite: true,
+//     centerPadding: "100px",
+//     slidesToShow: 8,
+//     slidesToScroll: 1,
+//     initialSlide: 0,
+//     // autoplay: true,
+//     autoplaySpeed: 3000,
+//     pauseOnHover: true,
+//     responsive: [
+//       {
+//         breakpoint: 1700,
+//         settings: {
+//           slidesToShow: 7,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1540,
+//         settings: {
+//           centerPadding: "60px",
+//           slidesToShow: 7,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1470,
+//         settings: {
+//           centerPadding: "100px",
+//           slidesToShow: 6,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1360,
+//         settings: {
+//           centerPadding: "60px",
+//           slidesToShow: 6,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1260,
+//         settings: {
+//           centerPadding: "100px",
+//           slidesToShow: 5,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1160,
+//         settings: {
+//           centerPadding: "50px",
+//           slidesToShow: 5,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 1020,
+//         settings: {
+//           centerPadding: "100px",
+//           slidesToShow: 4,
+//           slidesToScroll: 1,
+//         },
+//       },
+//       {
+//         breakpoint: 980,
+//         settings: {
+//           centerPadding: "40px",
+//           slidesToShow: 4,
+//           slidesToScroll: 1,
+//         },
+//       },
+//     ],
+//   };
+
+//   // --------spinner
+//   const Load = loader ? (
+//     <div className="main-slider__element">
+//       <div className="main-slider__card">
+//         <img
+//           className="slider"
+//           src={spinner}
+//           alt="spin"
+//           style={{
+//             position: "absolute",
+//             left: "50%",
+//             top: "40%",
+//             display: "block",
+//             width: "100px",
+//             height: "100px",
+//           }}
+//         />
+//       </div>
+//     </div>
+//   ) : null;
+
+//   // -----------render slider------
+//   const animeRender = (arr) => {
+//     return arr.map((data, i) => (
+//       <div className="main-slider__element">
+//         <div className="main-slider__card">
+//           {Load}
+//           {<Wiev data={data} key={i} />}
 //         </div>
 //       </div>
 //     ));
 //   };
 
-//   const Wiev = ({ elem }) => {
-//     return (
-//       <img className="main-slider__img" src={elem.poster} alt={elem.title} />
-//     );
-//   };
+//   const AnimeSlides = animeRender(anime);
+
 //   return (
-//     <div className="slider-container ">
+//     <div className="slider-container  main-slider">
 //       <Slider {...settings}>
-//         {slide.map((elem) => (
-//           <div className="main-slider__card">
-//             {Load}
-//             {/* {<Wiev elem={elem} />} */}
-//           </div>
-//         ))}
+//         {/* {Load} */}
+//         {AnimeSlides}
 //       </Slider>
 //     </div>
+//   );
+// };
+// // ---------content--------
+// const Wiev = ({ data, loader }) => {
+//   // console.log("render");
+//   return (
+//     <img className="main-slider__img" src={data.poster} alt={data.title} />
 //   );
 // };
 
